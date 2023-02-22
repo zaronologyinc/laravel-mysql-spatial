23f83a0b6b9de43be6aee50a65e003a2cfb3f806
davidvdtak <72360343+davidvdtak@users.noreply.github.com>
2023-02-22 17:08:00 -0500

    Update MysqlConnection.php

--- src/MysqlConnection.php
+++ src/MysqlConnection.php
@@ -6,7 +6,6 @@ use Doctrine\DBAL\Types\Type as DoctrineType;
 use Grimzy\LaravelMysqlSpatial\Schema\Builder;
 use Grimzy\LaravelMysqlSpatial\Schema\Grammars\MySqlGrammar;
 use Illuminate\Database\MySqlConnection as IlluminateMySqlConnection;
-use Illuminate\Support\Facades\ParallelTesting;
 
 class MysqlConnection extends IlluminateMySqlConnection
 {
@@ -14,8 +13,10 @@ class MysqlConnection extends IlluminateMySqlConnection
     {
         parent::__construct($pdo, $database, $tablePrefix, $config);
 
-        if (class_exists(DoctrineType::class) && ! ParallelTesting::token()) {
-            $this->registerDoctrineTypeMappings();
+        if (class_exists(DoctrineType::class)) {
+            try {
+                $this->registerDoctrineTypeMappings();
+            } catch(\Exception $e){}
         }
     }
 
